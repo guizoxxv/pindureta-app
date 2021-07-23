63defcc556c31f1dce5a742f53c389b0bedcbcb6
guizoxxv <guizoxxv@gmail.com>
2021-07-23 00:51:58 -0300

    Updating dependencies

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const appName = 'pinduretaFront';
-export const apiBaseURL = 'http://localhost:3001';
+export const appName = 'pindureta';
+export const apiBaseURL = 'http://localhost:3000';
 export const flashMessageStorageId = `@${appName}:flashMessage`;
