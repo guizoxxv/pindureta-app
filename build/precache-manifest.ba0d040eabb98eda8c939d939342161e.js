3c6256bd5078371b39f587a66cb3f288375ca755
guizoxxv <guizoxxv@gmail.com>
2021-07-23 09:29:43 -0300

    Build prod

---
+++ build/precache-manifest.ba0d040eabb98eda8c939d939342161e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92dee04b0611a40f7d5f9d4a0d323c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d42ef81ea1b59c57ff4c",
+    "url": "/static/css/2.14f3cd7d.chunk.css"
+  },
+  {
+    "revision": "b5e61b067f460b819dd1",
+    "url": "/static/css/main.93da2462.chunk.css"
+  },
+  {
+    "revision": "d42ef81ea1b59c57ff4c",
+    "url": "/static/js/2.f897b2dc.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.f897b2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad695425d07e7c5dd23",
+    "url": "/static/js/3.2482e936.chunk.js"
+  },
+  {
+    "revision": "b5e61b067f460b819dd1",
+    "url": "/static/js/main.03a20889.chunk.js"
+  },
+  {
+    "revision": "cbb640eeb7647d252456",
+    "url": "/static/js/runtime-main.cb1fe224.js"
+  }
+]);
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export const appName = 'pindureta';
 export const apiBaseURL = 'http://localhost:3000';
+// export const apiBaseURL = 'https://pindureta-api.guizo.dev.br'; // prod
 export const flashMessageStorageId = `@${appName}:flashMessage`;
